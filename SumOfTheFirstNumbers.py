5a357b8968b70d84e77cd236a23be6f1aaf17cf0
Bruno Perez <brucorreo166@gmail.com>
2022-08-28 09:48:29 -0500

    The new excersice

---
+++ SumOfTheFirstNumbers.py
@@ -0,0 +1,12 @@
+#
+##The sum of the fisrt numbers
+#
+
+#Read the input from the user
+number=int(input("Give me a number: "))
+
+#Compute the numbers
+sum=(number*(number+1))/2
+
+#Display the result of the sum
+print("The result of your sum is:",sum)
